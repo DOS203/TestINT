95364d5e69dea4395c829c5f4293aef23377c55b
dos203 <dinoshanr@gmail.com>
2019-09-19 14:20:18 +1000

    fix mm

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-
-
-
-
-var indexRouter = require('./routes/index');
 
 const app = express();
 //DB config
@@ -20,28 +14,20 @@ const db = require('./config/database');
 
 // Load routes
 const users = require('./routes/users');
-const payment = require('./routes/payment');
+
 const delivery = require('./routes/delivery');
-
-
 
 // Passport Config
 require('./config/passport')(passport);
 
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
-
-
 // Connect to mongoose
-
-
-
-mongoose.connect('mongodb://127.0.0.1:27017/shopping', {useNewUrlParser: true});
-
-// puttt 
-
-  
-
+mongoose.connect(db.mongoURI, {
+  useMongoClient: true
+})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -52,9 +38,6 @@ app.set('view engine', 'handlebars');
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -85,21 +68,19 @@ app.use(function(req, res, next){
 });
 
 // Index Route
-//app.get('/', (req, res) => {
- // const title = 'Welcome';
- //res.render('index', {
- // title: title
-//  });
-//});
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
+});
 
 // About Route
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.get('/payment', (req, res) => {
-  res.render('payment');
-});
+
 
 app.get('/delivery', (req, res) => {
   res.render('delivery');
@@ -112,18 +93,11 @@ app.get('/courier', (req, res) => {
 app.get('/deliveryDT', (req, res) => {
   res.render('deliveryDT');
 });
-app.get('/deliveryFree', (req, res) => {
-  res.render('deliveryFree');
-});
 
 // Use routes
 app.use('/users', users);
 
 app.use('/delivery', delivery);
-
-app.use('/payment', payment);
-
-app.use('/', indexRouter);
 
 //Load 404 page (if page is not exist!)
 app.use((req ,res) => res.render('not_found'));
@@ -134,5 +108,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () =>{
   console.log(`Server started on port ${port}`);
 });
-
-module.exports = app;
