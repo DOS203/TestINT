e1c30d35b0ed44a2436f87d0c203a3a082a45a01
dos203 <dinoshanr@gmail.com>
2019-10-01 14:32:41 +1000

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV == 'production'){
     module.exports = {mongoURI:
-        'mongodb://newdown:newdown1234@ds263927.mlab.com:63927/heroku_fw9f58vn'}
+        'mongodb://users:Test123@ds157946.mlab.com:57946/heroku_tlms4x2k'}
 
 }else{
     module.exports = {mongoURI: 'mongodb://127.0.0.1:27017/LocalDev'}
