2a1a4b115944ef152b3e028f3696e4cea9931947
dos203 <dinoshanr@gmail.com>
2019-10-18 10:20:01 +1100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV == 'production'){
     module.exports = {mongoURI:
-        'mongodb://dino:dino203@ds163517.mlab.com:63517/heroku_c4k6ns9h'}
+        'mongodb://newdown:newdown1234@ds263927.mlab.com:63927/heroku_fw9f58vn'}
 
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
